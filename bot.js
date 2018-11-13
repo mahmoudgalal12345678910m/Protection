6916280f07ea1734a1ed2d379f38ceeb7d04634f
kingofmenewwork <43719252+mahmoudgalal12345678910m@users.noreply.github.com>
2018-11-13 11:07:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = "-"
-const slowmode_mentions = new Map();
-const slowmode_links = new Map();
-const slowmode_attachments = new Map();
-const ratelimit = 7500; // within 7.5 seconds
-const logChannel = "511826404136976394";
- 
 
 client.user.setGame('Mal الحامى لسيرفر ','https://www.twitch.tv/sytra_ayman');
   console.log(`Logged in as ${client.user.tag}!`);
@@ -120,120 +114,6 @@ if(c) return;
 if(c) return c.send(`:no_entry: | Banned ${member} , Reason: \`AntiBots - Protection\``);
 }
 });
-
-
-client.on("message", message => {
- 
-    if (message.content.startsWith("!ping")) {
-        let startTime = Date.now();
-        message.channel.send("Ping...").then(newMessage => {
-            let endTime = Date.now();
-            newMessage.edit("Pong! Took `" + Math.round(endTime - startTime) + "ms`!");
-        });
-    }
- 
-    function log(logmessage) {
-        if (message.guild.channels.has(logChannel)) {
-            message.guild.channels.get(logChannel).send({ embed: logmessage}).then().catch(err => console.log(err));
-        }
-    }
- 
-   
-    let banLevel = {
-        "mentions": 10,
-        "links": 10,
-        "attachments": 10
-    };
- 
-   
-    if (message.author.bot || !message.guild || !message.member || !message.guild.member(client.user).hasPermission("BAN_MEMBERS") || message.member.hasPermission("MANAGE_MESSAGES")) return;
- 
- 
-    if (message.mentions.users.size == 1 && message.mentions.users.first().bot) return;
- 
-   
-    let entry_mentions = slowmode_mentions.get(message.author.id);
-    let entry_links = slowmode_links.get(message.author.id);
-    let entry_attachments = slowmode_attachments.get(message.author.id);
- 
-    if (!entry_mentions) {
-        entry_mentions = 0;
-        slowmode_mentions.set(message.author.id, entry_mentions);
-    }
-    if (!entry_links) {
-        entry_links = 0;
-        slowmode_links.set(message.author.id, entry_links);
-    }
-    if (!entry_attachments) {
-        entry_attachments = 0;
-        slowmode_attachments.set(message.author.id, entry_attachments);
-    }
- 
-   
-    entry_mentions += message.mentions.users.size + message.mentions.roles.size;
-    entry_links += message.embeds.length;
-    entry_attachments += message.attachments.size;
-   
-    slowmode_mentions.set(message.author.id, entry_mentions);
-    slowmode_links.set(message.author.id, entry_links);
-    slowmode_attachments.set(message.author.id, entry_attachments);
- 
-   
-    if (entry_links > banLevel.links) {
-        message.member.ban(1).then(member => {
-            message.channel.send(`:ok_hand: banned \`${message.author.tag}\` for \`link spam\``);
-            log(new Discord.RichEmbed().setTitle(':hammer: Banned').setColor(0xFF0000).setTimestamp().addField('User', `${message.author.tag} (${message.author.id})`).addField('Reason', `Posting too many links (${entry_links}x)`));
-            slowmode_links.delete(message.author.id);
-        })
-        .catch(e => {
-            log(new Discord.RichEmbed().setTitle(':x: ERROR').setColor(0x000001).setTimestamp().addField('User', `${message.author.tag} (${message.author.id})`).addField('Reason', `Could not ban because they have a higher role`));
-        });
-    } else {
-        setTimeout(()=> {
-            entry_links -= message.embeds.length;
-            if(entry_links <= 0) slowmode_links.delete(message.author.id);
-        }, ratelimit);
-    }
- 
-    if (entry_mentions > banLevel.mentions) {
-        message.member.ban(1).then(member => {
-            message.channel.send(`:ok_hand: banned \`${message.author.tag}\` for \`mention spam\``);
-            log(new Discord.RichEmbed().setTitle(':hammer: Banned').setColor(0xFF0000).setTimestamp().addField('User', `${message.author.tag} (${message.author.id})`).addField('Reason', `Mentioning too many users (${entry_mentions}x)`));
-            slowmode_mentions.delete(message.author.id);
-        })
-        .catch(e => {
-            log(new Discord.RichEmbed().setTitle(':x: ERROR').setColor(0x000001).setTimestamp().addField('User', `${message.author.tag} (${message.author.id})`).addField('Reason', `Could not ban because they have a higher role`));
-        });
-    } else {
-        setTimeout(()=> {
-            entry_mentions -= message.mentions.users.size + message.mentions.roles.size;
-            if(entry_mentions <= 0) slowmode_mentions.delete(message.author.id);
-        }, ratelimit);
-    }
- 
-    if (entry_attachments > banLevel.attachments) {
-        message.member.ban(1).then(member => {
-            message.channel.send(`:ok_hand: banned \`${message.author.tag}\` for \`image spam\``);
-            log(new Discord.RichEmbed().setTitle(':hammer: Banned').setColor(0xFF0000).setTimestamp().addField('User', `${message.author.tag} (${message.author.id})`).addField('Reason', `Posting too many images (${entry_attachments}x)`));
-            slowmode_attachments.delete(message.author.id);
-        })
-        .catch(e => {
-            log(new Discord.RichEmbed().setTitle(':x: ERROR').setColor(0x000001).setTimestamp().addField('User', `${message.author.tag} (${message.author.id})`).addField('Reason', `Could not ban because they have a higher role`));
-        });
-    } else {
-        setTimeout(()=> {
-            entry_attachments -= message.attachments.size;
-            if(entry_attachments <= 0) slowmode_attachments.delete(message.author.id);
-        }, ratelimit);
-    }
- 
-});
- 
-process.on("unhandledRejection", err => {
-    console.error("Uncaught Promise Error: \n" + err.stack);
-});
-
-
 
 
 
